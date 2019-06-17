4a62a15102f26069184e3d0ff97678dcd1a3028c
Dan Carroll <dan@sputnik.net.au>
2019-06-17 10:14:56 +1000

    Added GPLv3 License.

--- api/1/jwt.php
+++ api/1/jwt.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ * Copyright (c) 2019 Dan Carroll
+ *
+ * This file is part of FlightLine.
+ *
+ * FlightLine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * FlightLine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FlightLine.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 /**
  * JSON Web Token implementation, based on this spec:
